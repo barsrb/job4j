6f49d75368473d3df8ed92f0cebdde41ca432c39
Mikhail Pershukov <dev@76543210.ru>
2020-02-11 00:01:10 +0300

    5.8. JavaDoc. Code convention[#230766]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,29 +1,61 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculator - класс для простых вычислений из задания 3.3
+ * @author Mikhail Pershukov
+ * @since 30.01.2020
+ */
 public class Calculator {
+
+    /**
+     * Метод сложения. Выводит на экран сумму.
+     * @param first первое слогаемое
+     * @param second второе слогаемое
+     */
     public static void add (double first, double second) {
         double result = first + second;
         System.out.println(first + " + " + second + " = " + result);
 
     }
 
+    /**
+     * Метод деления. Выводит на экран частное.
+     * @param first делимое
+     * @param second делитель
+     */
     public static void div (double first, double second) {
         double result = first / second;
         System.out.println(first + " / " + second + " = " + result);
 
     }
 
+    /**
+     * Метод умножения. Выводит на экран произведение.
+     * @param first первый множитель
+     * @param second второй множитель
+     */
     public static void multiply (double first, double second) {
         double result = first * second;
         System.out.println(first + " * " + second + " = " + result);
 
     }
 
+
+    /**
+     * Метод вычитания. Выводит на экран разности.
+     * @param first уменьшаемое
+     * @param second вычитаемое
+     */
     public static void subtrack (double first, double second) {
         double result = first - second;
         System.out.println(first + " - " + second + " = " + result);
 
     }
+
+    /**
+     * Точка входа в программу. Метод для тестирования класса.
+     * @param args - агрументы
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Mikhail Pershukov (dev@76543210.ru)
+ * @version 0.1
+ * @since 10.02.2020
+ */
+package ru.job4j.calculator;
