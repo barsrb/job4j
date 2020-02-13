db20aa3bee30d2ffac93e08e3ba5004b549f660f
Mikhail Pershukov <dev@76543210.ru>
2020-02-13 17:07:01 +0300

    Объединить два массива [#124482]

---
+++ chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -0,0 +1,31 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Merge {
+
+    public int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int leftPointer = 0;
+        int rightPointer = 0;
+        int resultPointer = 0;
+
+        while (resultPointer < rsl.length) {
+            if (leftPointer == left.length) {
+                rsl[resultPointer] = right[rightPointer];
+                rightPointer++;
+            } else if (rightPointer == right.length) {
+                rsl[resultPointer] = left[leftPointer];
+                leftPointer++;
+            } else if (left[leftPointer] < right[rightPointer]) {
+                rsl[resultPointer] = left[leftPointer];
+                leftPointer++;
+            } else {
+                rsl[resultPointer] = right[rightPointer];
+                rightPointer++;
+            }
+            resultPointer++;
+        }
+        return rsl;
+    }
+}
