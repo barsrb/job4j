1c672f74d29011be46ec6c5237781cd16e732000
Mikhail Pershukov <dev@76543210.ru>
2020-02-21 18:09:49 +0300

    5. Реализовать класс Tracker[#230881]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,47 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
+    private final Item[] items = new Item[100];
+
+    private int position = 0;
+
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        items[this.position++] = item;
+        return item;
+    }
+
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, position);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] tempResults = new Item[100];
+        int count = 0;
+        for (int index = 0; index < position; index++) {
+            if (items[index].getName().equals(key)) {
+                tempResults[count++] = items[index];
+            }
+        }
+        return Arrays.copyOf(tempResults, count);
+    }
+
+    public Item findById(String id) {
+        for (int index = 0; index < position; index++) {
+            if (items[index].getId().equals(id)) {
+                return  items[index];
+            }
+        }
+        return null;
+    }
+
+
 }
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,74 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class TrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void addThreeItemsAndGetSecond() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1");
+        Item item2 = new Item("test2");
+        Item item3 = new Item("test3");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item result = tracker.findById(item2.getId());
+        assertThat(result.getName(), is(item2.getName()));
+    }
+
+    @Test
+    public void addFourItemsAndGetTwoByName() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1");
+        Item item2 = new Item("testname");
+        Item item3 = new Item("test3");
+        Item item4 = new Item("testname");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        Item[] results = tracker.findByName("testname");
+        Item[] expect = {item2, item4};
+        assertThat(results, is(expect));
+    }
+
+    @Test
+    public void addThreeItemsButNotFoundByID() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1");
+        Item item2 = new Item("test2");
+        Item item3 = new Item("test3");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item result = tracker.findById("none");
+        assertNull(result);
+    }
+
+    @Test
+    public void addFourItemsAndGetAll() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1");
+        Item item2 = new Item("testname");
+        Item item3 = new Item("test3");
+        Item item4 = new Item("testname");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        Item[] results = tracker.findAll();
+        Item[] expect = {item1, item2, item3, item4};
+        assertThat(results, is(expect));
+    }
+}
