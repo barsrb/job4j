ff806c635efb4e0c8438e2877112f2f72691ea45
Mikhail Pershukov <dev@76543210.ru>
2020-02-23 14:18:30 +0300

    3. Интерфейс Input[#230827]

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,42 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    int fuel = 0;
+    int passengers = 0;
+
+    @Override
+    public void go() {
+        if(fuel>0) {
+            System.out.println("Wheels of the bus go round and round...");
+            System.out.println("There are " + passengers + " in bus!");
+            System.out.println("There are " + fuel + " in tank.");
+            fuel--;
+        }
+        else {
+            System.out.println("No fuel. Refill and go!");
+        }
+    }
+
+    @Override
+    public void passengers(int passengersCount) {
+        this.passengers = passengersCount;
+    }
+
+    @Override
+    public double refuel(int liters) {
+        double price = 9.99;
+        fuel += liters;
+        return price*liters;
+    }
+
+    public static void main(String[] args) {
+        Bus bus = new Bus();
+        bus.go();
+        bus.passengers(10);
+        System.out.println("Paid " + bus.refuel(3) + "$ for 3 liters.");
+        bus.go();
+        bus.go();
+        bus.go();
+        bus.go();
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,11 @@
+package ru.job4j.poly;
+
+public interface Transport {
+
+    public void go();
+
+    public void passengers(int passengersCount);
+
+    public double refuel (int liters);
+
+}
