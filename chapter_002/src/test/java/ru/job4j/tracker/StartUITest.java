36bbf6a1d69770b91e87320d26b3bd7a7a701ee4
Mikhail Pershukov <dev@76543210.ru>
2020-02-26 12:36:37 +0300

    9. Написать тесты на StartUI.[#230837]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -14,4 +14,28 @@ public class StartUITest {
         new StartUI().init(input, new Tracker(), new UserAction[] {action});
         assertThat(action.isCall(), is(true));
     }
+
+    @Test
+    public void whenSelectSecondAction() {
+        StubInput input = new StubInput(
+                new String[] {"1"}
+        );
+        StubAction action1 = new StubAction();
+        StubAction action2 = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] {action1, action2});
+        assertThat(action1.isCall(), is(false));
+        assertThat(action2.isCall(), is(true));
+    }
+
+    @Test
+    public void whenSelectFirstAction() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action1 = new StubAction();
+        StubAction action2 = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] {action1, action2});
+        assertThat(action1.isCall(), is(true));
+        assertThat(action2.isCall(), is(false));
+    }
 }
