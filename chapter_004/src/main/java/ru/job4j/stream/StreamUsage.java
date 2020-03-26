d42fb67fb0474d838b393a4513599df36d68ed4d
Mikhail Pershukov <dev@76543210.ru>
2020-03-26 23:17:15 +0300

    0. Stream API[#230656] Checkstyle

--- chapter_004/src/main/java/ru/job4j/stream/StreamUsage.java
+++ chapter_004/src/main/java/ru/job4j/stream/StreamUsage.java
@@ -29,10 +29,9 @@ public class StreamUsage {
 
         @Override
         public String toString() {
-            return "Task{" +
-                    "name='" + name + '\'' +
-                    ", spent=" + spent +
-                    '}';
+            return "Task{"
+                    + "name='" + name + '\''
+                    + ", spent=" + spent + '}';
         }
 
         public String getName() {
--- chapter_004/src/test/java/ru/job4j/stream/StreamUsageTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/StreamUsageTest.java
@@ -11,11 +11,11 @@ import static org.junit.Assert.*;
 public class StreamUsageTest {
 
     @Test
-    public void getBugsTest(){
+    public void getBugsTest() {
         StreamUsage.Task task1 = new StreamUsage.Task("Bug #1", 100);
         StreamUsage.Task task2 = new StreamUsage.Task("Task #2", 100);
         StreamUsage.Task task3 = new StreamUsage.Task("Bug #3", 100);
-        StreamUsage su = new StreamUsage(Arrays.asList(task1, task2, task3 ));
+        StreamUsage su = new StreamUsage(Arrays.asList(task1, task2, task3));
         List<StreamUsage.Task> expected = Arrays.asList(task1, task3);
         List<StreamUsage.Task> bugs = su.getBugs();
         assertThat(expected, is(bugs));
@@ -23,22 +23,22 @@ public class StreamUsageTest {
 
 
     @Test
-    public void getTaskNamesTest(){
+    public void getTaskNamesTest() {
         StreamUsage.Task task1 = new StreamUsage.Task("Bug #1", 100);
         StreamUsage.Task task2 = new StreamUsage.Task("Task #2", 100);
         StreamUsage.Task task3 = new StreamUsage.Task("Bug #3", 100);
-        StreamUsage su = new StreamUsage(Arrays.asList(task1, task2, task3 ));
+        StreamUsage su = new StreamUsage(Arrays.asList(task1, task2, task3));
         List<String> expected = Arrays.asList(task1.getName(), task2.getName(), task3.getName());
         List<String> names = su.getTaskNames();
         assertThat(expected, is(names));
     }
 
     @Test
-    public void countSpentTest(){
+    public void countSpentTest() {
         StreamUsage.Task task1 = new StreamUsage.Task("Bug #1", 100);
         StreamUsage.Task task2 = new StreamUsage.Task("Task #2", 10);
         StreamUsage.Task task3 = new StreamUsage.Task("Bug #3", 1000);
-        StreamUsage su = new StreamUsage(Arrays.asList(task1, task2, task3 ));
+        StreamUsage su = new StreamUsage(Arrays.asList(task1, task2, task3));
         long expected = 1110L;
         long spentCount = su.countSpent();
         assertThat(expected, is(spentCount));
